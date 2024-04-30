992b3ba7c6e8915e8f2ef067d9098f6ba261286a
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-04-30 18:19:25 +0000

    ssssssssssssssssssssssssssss

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+function checking() {
+  const usernumber = parseInt(document.getElementById("number").value)
+  const day = document.getElementById("day").value
+  if ((day == "Tuesday")|| (day == "Thurday")|| (usernumber>12)&& (usernumber<21) )  {
+    document.getElementById("result").innerHTML = "you are eligible student for pricing!"
+  }
+  else {
+    document.getElementById("result").innerHTML = "you must pay regular price"
+  }
+}
